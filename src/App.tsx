ce5e2638c1f841f9de505c70f0e9a9ca9fe1493f
Your Name <your.email@example.com>
2026-02-04 13:58:22 +0530

    Remove Slide6 'How SwiftMart Works' completely from presentation

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Slide2 from './components/Slide2';
 import Slide3 from './components/Slide3';
 import Slide4 from './components/Slide4';
 import Slide5 from './components/Slide5';
-import Slide6 from './components/Slide6';
 import Slide7 from './components/Slide7';
 import Slide8 from './components/Slide8';
 import Slide9 from './components/Slide9';
@@ -15,7 +14,7 @@ export default function App() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [touchStart, setTouchStart] = useState(0);
   const [touchEnd, setTouchEnd] = useState(0);
-  const totalSlides = 10;
+  const totalSlides = 9;
 
   const slides = [
     <Slide1 key="slide1" />,
@@ -23,7 +22,6 @@ export default function App() {
     <Slide3 key="slide3" />,
     <Slide4 key="slide4" />,
     <Slide5 key="slide5" />,
-    <Slide6 key="slide6" />,
     <Slide7 key="slide7" />,
     <Slide8 key="slide8" />,
     <Slide9 key="slide9" />,
